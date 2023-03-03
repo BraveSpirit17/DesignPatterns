3ebeb7e89115bbbf48cd21878195120a643b6b86
Brave Spirit <ilavlasenko9@gmail.com>
2023-03-03 11:54:01 +0200

    Added library for Strategy

---
+++ Strategy/ClaimContext.cs
@@ -0,0 +1,25 @@
+﻿namespace Strategy;
+
+public class ClaimContext
+{
+    private IClaimStrategy _strategy;
+
+    public ClaimContext()
+    {
+    }
+
+    public ClaimContext(IClaimStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+    
+    public void SetStrategy(IClaimStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public Claim ExecuteStrategy(string systemKey)
+    {
+        return _strategy.CreateApplication(systemKey);
+    }
+}
